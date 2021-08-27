59127e872ae802293c952c1371a1d7f623bdf6fd
adamilnicki <adamilnicki00@gmail.com>
2021-08-27 13:18:29 +0200

    slider

--- js/app.js
+++ js/app.js
@@ -1,35 +1,29 @@
-// immediate invoked function expression
+let images = [
+  "contBcg-0.jpeg",
+  "contBcg-1.jpeg",
+  "contBcg-2.jpeg",
+  "contBcg-3.jpeg",
+  "contBcg-4.jpeg",
+];
 
-(function() {
-  const pictures = [
-    "contBcg-0",
-    "contBcg-1",
-    "contBcg-2",
-    "contBcg-3",
-    "contBcg-4"
-  ];
+let btnLeft = document.querySelector(".btn-left");
+let btnRight = document.querySelector(".btn-right");
+let imgContainer = document.querySelector(".img-container");
+let i = 0;
 
-  //select both left and right button. Add event listeners
-  const buttons = document.querySelectorAll('.btn')
-  const imageDIV = document.querySelector('.img-container')
-  let counter = 0
-
-  buttons.forEach(function(button){
-    button.addEventListener('click', function(e){
-      if (button.classList.contains('btn-left')){
-        counter--
-        if(counter < 0){
-          counter = pictures.length -1
-        }
-        imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
-      }
-      if (button.classList.contains('btn-right')){
-        counter++
-        if(counter > pictures.length -1){
-          counter = 0
-        }
-        imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
-      }
-    })
-  })
-})();
+btnRight.addEventListener("click", function () {
+  if (i < 4) {
+    i++;
+  } else {
+    i = 0;
+  }
+  imgContainer.style.backgroundImage = `url('./img/contBcg-${i}.jpeg')`;
+});
+btnLeft.addEventListener("click", function () {
+  if (i > 0) {
+    i--;
+  } else {
+    i = 4;
+  }
+  imgContainer.style.backgroundImage = `url('./img/contBcg-${i}.jpeg')`;
+});
